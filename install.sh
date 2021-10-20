336541bdafcda88825a507e2602f81785a229b9c
ReallyLiri <reallyliri@gmail.com>
2021-10-20 16:38:07 +0300

    Updated install script

--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@ echo "Installing..."
 
 TMPDIR=${TMPDIR:-"/tmp"}
 pushd "$TMPDIR" > /dev/null
-  mkdir -p gitsnap
-  pushd gitsnap > /dev/null;
+  mkdir -p kubescout
+  pushd kubescout > /dev/null;
     distro=$(if [[ "$(uname -s)" == "Darwin" ]]; then echo "osx"; else echo "linux"; fi)
     if [ -n "$1" ]
     then
       echo "Will download and install v$1"
-      curl -sSL --fail -o gitsnap.zip "https://github.com/apiiro/kubescout/releases/download/v$1/gitsnap-$1-$distro.zip"
+      curl -sSL --fail -o kubescout.zip "https://github.com/reallyliri/kubescout/releases/download/v$1/kubescout-$distro.zip"
     else
-      curl -s --fail https://api.github.com/repos/apiiro/kubescout/releases/latest | grep "browser_download_url.*$distro.zip" | cut -d : -f 2,3 | tr -d \" | xargs curl -sSL --fail -o gitsnap.zip
+      curl -s --fail https://api.github.com/repos/reallyliri/kubescout/releases/latest | grep "browser_download_url.*$distro.zip" | cut -d : -f 2,3 | tr -d \" | xargs curl -sSL --fail -o kubescout.zip
     fi
-    unzip gitsnap.zip
-    chmod +x gitsnap-*
-    cp -f gitsnap-* /usr/local/bin/kubescout
+    unzip kubescout.zip
+    chmod +x kubescout
+    cp -f kubescout /usr/local/bin/kubescout
   popd > /dev/null
-  rm -rf gitsnap
+  rm -rf kubescout
 popd > /dev/null
 
 echo "Done: $(kubescout -v)"
